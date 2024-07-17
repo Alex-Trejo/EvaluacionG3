802e8e9cc1578eeef5119c8d80612cb9ca316aee
KarlaAns <kansatunauemee@gmail.com>
2024-07-16 23:57:14 -0500

    added stuff

--- src/components2/HomePage/TopHomePage.js
+++ src/components2/HomePage/TopHomePage.js
@@ -18,18 +18,17 @@ export default function TopHomePage() {
           }}
         />
         {/* Contenido principal */}
-        <div className="relative z-40 felx mx-auto max-w-2xl justify-end py-20 sm:py-32 lg:py-40 items-center text-white">
+        <div className="relative z-40 max-w-200 justify-end py-20 sm:py-32 lg:py-40 text-white">
           {/* Etiqueta y texto */}
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Clothing Shop.
             </div>
           </div>
-          <div className='ml-auto text-end pr-12'>
-            <h1 className="text-4xl font-bold tracking-tight sm:text-6xl text-black ring-1 z-index">
-              La mejor calidad en ropa solo para ti
+          <div className='text-center sm:text-end'>
+            <h1 className="text-4xl font-bold tracking-tight sm:text-6xl text-black z-index">
+              The best quality in clothes just for you
             </h1>
-          
           </div>
         </div>
       </div>
